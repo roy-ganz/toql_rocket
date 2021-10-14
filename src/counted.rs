4bca28967c9e11eca43d383bcd6470cc40ec5107
roy-ganz <roy.ganz@glanis.biz>
2021-10-14 16:47:29 +0200

    Support PageCounts

--- src/counted.rs
+++ src/counted.rs
@@ -2,16 +2,17 @@ use rocket::http::Status;
 use rocket::Response;
 use rocket::Request;
 use rocket::response::Responder;
+use toql::prelude::PageCounts;
 
 /// Struct that keeps a result with counting information.
 #[derive( Debug)]
-pub struct Counted<R>(pub R, pub Option<(u64, u64)>);
+pub struct Counted<R>(pub R, pub Option<PageCounts>);
 
 /// Creates a response and puts the counting information
-/// into the HTTP response headers, if there is some value.
+/// into the HTTP response headers, if there is any.
 /// 
-/// The header `X-Total-Count` contains the first value of the tuple in [Counted].
-/// The header `X-Filtered-Count` contains the second value of the tuple in [Counted].
+/// The header `X-Total-Count` contains the title value of the page counts in [Counted].
+/// The header `X-Filtered-Count` contains the filtered value of the page counts in [Counted].
 impl<'r, R: Responder<'r, 'static>> Responder<'r, 'static> for Counted<R> 
 {
     fn respond_to(self, req: &'r Request<'_>) -> Result<Response<'static>, Status> {
@@ -19,9 +20,9 @@ impl<'r, R: Responder<'r, 'static>> Responder<'r, 'static> for Counted<R>
         let responder = self.0;
         build.merge(responder.respond_to(req)?);
 
-        if let Some((filtered_count, total_count)) = self.1 {
-            build.raw_header("X-Total-Count", total_count.to_string());
-            build.raw_header("X-Filtered-Count", filtered_count.to_string());
+        if let Some(PageCounts{filtered, total}) = self.1 {
+            build.raw_header("X-Total-Count", total.to_string());
+            build.raw_header("X-Filtered-Count", filtered.to_string());
         }
            
          build.ok()
